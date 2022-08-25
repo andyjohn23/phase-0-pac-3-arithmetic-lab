11977dc1d17b36f5980f70f7697f98f61790afbb
andyjohn23 <andrewnick906@gmail.com>
2022-08-25 10:23:47 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+};
+add(3, 3);
+
+function subtract(a, b) {
+    return a - b;
+};
+subtract(4, 3);
+
+function multiply(a, b) {
+    return a * b;
+};
+multiply(4, 3);
+
+function divide(a, b) {
+    return a / b;
+};
+divide(2, 2)
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    return parseFloat(n);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
